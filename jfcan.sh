f7608030063e309e1edd71a13edc99af0004ef9c
Donald Hoskins <grommish@gmail.com>
2013-12-27 10:58:40 -0500

    Clean up of Scripts, Auto-Generate .md5 file

--- jfcan.sh
+++ jfcan.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 source build/envsetup.sh
 make installclean
-BDATE=(date +%m-%d)
+BDATE=`date +%m-%d`
 COPY_DIR=/home/copy/shares/shares/dubbsy/Carrier_ROMs
+if [ ! -d "${COPY_DIR}/${BDATE}" ]; then
+	echo "Creating directory for ${COPY_DIR}/${BDATE}"
+	mkdir -p ${COPY_DIR}/${BDATE}
+fi
+
 # jfltecan
 lunch oct_jfltecan-userdebug && make installclean && make otapackage -j21
 find out/target/product  '(' -name '*ota-eng*.zip' -size +100000k -print0 ')' |
         xargs --null md5sum |
         while read CHECKSUM FILENAME
         do
-                echo "${CHECKSUM} ${FILENAME}" >> "${FILENAME}.desc"
-                mv ${FILENAME} ${COPY_DIR}/Oct_${BDATE}
-                mv "${FILENAME}.desc" ${COPY_DIR}/Oct_${BDATE}
+                echo "${CHECKSUM} ${FILENAME}" >> "${FILENAME}.md5"
+                mv -f ${FILENAME} ${COPY_DIR}/${BDATE}
+                mv -f "${FILENAME}.md5" ${COPY_DIR}/${BDATE}
         done
 
