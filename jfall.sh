69c936e06b69b7eabea2bd222c42d83e789dc859
Donald Hoskins <grommish@gmail.com>
2013-12-27 03:03:13 -0500

    Cleaned up Automation Scripts

--- jfall.sh
+++ jfall.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 source build/envsetup.sh
 make installclean
-#ATT
-lunch oct_jflteatt-userdebug && make installclean && make otapackage -j21
-#CAN
-lunch oct_jfltecan-userdebug && make installclean && make otapackage -j21
-#CRI
-lunch oct_jfltecri-userdebug && make installclean && make otapackage -j21
-#CSP
-lunch oct_jfltecsp-userdebug && make installclean && make otapackage -j21
-#SPR
-lunch oct_jfltespr-userdebug && make installclean && make otapackage -j21
-#TMO
-lunch oct_jfltetmo-userdebug && make installclean && make otapackage -j21
-#USC
-lunch oct_jflteusc-userdebug && make installclean && make otapackage -j21
+BDATE=(date +%m-%d)
+COPY_DIR=/home/copy/shares/shares/dubbsy/Carrier_ROMs
+
+
 #VZW
-lunch oct_jfltevzw-userdebug && make installclean && make otapackage -j21
+lunch oct_jfltevzw-userdebug && make otapackage -j21
+#SPR
+lunch oct_jfltespr-userdebug && make otapackage -j21
+#USC
+lunch oct_jflteusc-userdebug && make otapackage -j21
 #XX
-lunch oct_jfltexx-userdebug && make installclean && make otapackage -j21
+lunch oct_jfltexx-userdebug && make otapackage -j21
+#ATT
+lunch oct_jflteatt-userdebug && make otapackage -j21
+#CAN
+lunch oct_jfltecan-userdebug && make otapackage -j21
+#CRI
+lunch oct_jfltecri-userdebug && make otapackage -j21
+#CSP
+lunch oct_jfltecsp-userdebug && make otapackage -j21
+#TMO
+lunch oct_jfltetmo-userdebug && make otapackage -j21
+
+
+find out/target/product  '(' -name '*.zip' -size +100000k -print0 ')' |
+        xargs --null md5sum |
+        while read CHECKSUM FILENAME
+        do
+                echo "${CHECKSUM} ${FILENAME}" >> "${FILENAME}.desc"
+                mv ${FILENAME} ${COPY_DIR}/Oct_${BDATE}
+                mv "${FILENAME}.desc" ${COPY_DIR}/Oct_${BDATE}
+        done
+
--- jfusc.sh
+++ jfusc.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 source build/envsetup.sh
 make installclean
+BDATE=(date +%m-%d)
+COPY_DIR=/home/copy/shares/shares/dubbsy/Carrier_ROMs
 # jflteusc
 lunch oct_jflteusc-userdebug && make installclean && make otapackage -j21
+find out/target/product  '(' -name '*.zip' -size +100000k -print0 ')' |
+        xargs --null md5sum |
+        while read CHECKSUM FILENAME
+        do
+                echo "${CHECKSUM} ${FILENAME}" >> "${FILENAME}.desc"
+                mv ${FILENAME} ${COPY_DIR}/Oct_${BDATE}
+                mv "${FILENAME}.desc" ${COPY_DIR}/Oct_${BDATE}
+        done
+
