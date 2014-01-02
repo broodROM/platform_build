9bfae827a5173f5dba20df635d18e9a37c33b9fa
Donald Hoskins <grommish@gmail.com>
2014-01-02 13:57:34 -0500

    Build Scripts: Inclusion of d2 (Galaxy S3) devices

--- d2all.sh
+++ d2all.sh
@@ -1,70 +1,107 @@
 #!/bin/bash
+## OctOs Automation Script
 source build/envsetup.sh
-make installclean
+
+## Current Build Date
 BDATE=`date +%m-%d`
-COPY_DIR=/home/copy/shares/shares/dubbsy/Carrier_ROMs
+## Check to see if there are build args
+## First Argument is for the -j concurrent build threads - Defaults to -j21 unless
+## you set it otherwise.  Careful, or it will melt your machine!
+if [ ! -z $1 ]; then
+        $1 = 21
+fi
+
+## Arg 2 is for Test vs Production.  True (y/1) means Test build, n/0 means Production
+## Defaults to Production
+if [ ! -z $2 ]; then
+        $1 = n
+fi
+
+TEST_BUILD=$2
+if [ TEST_BUILD ]; then
+COPY_DIR=/home/copy/shares/Octos/dubbsy/Carrier_ROMs/test_builds
+else
+COPY_DIR=/home/copy/shares/Octos/dubbsy/Carrier_ROMs
+fi
+
 if [ ! -d "${COPY_DIR}/${BDATE}" ]; then
         echo "Creating directory for ${COPY_DIR}/${BDATE}"
         mkdir -p ${COPY_DIR}/${BDATE}
 fi
 
-#ATT
-lunch oct_d2att-userdebug && make installclean && make otapackage -j21
-find out/target/product  '(' -name '*ota-eng*.zip' -size +100000k -print0 ')' |
+# d2vzw
+lunch oct_d2vzw-userdebug && make installclean && make otapackage -j$1
+find out/target/product '(' -name '*ota-eng*.zip' -size +100000k ')' -print0 |
         xargs --null md5sum |
         while read CHECKSUM FILENAME
         do
                 echo "${CHECKSUM} ${FILENAME}" >> "${FILENAME}.md5"
-                mv -f ${FILENAME} ${COPY_DIR}/${BDATE}
-                mv -f "${FILENAME}.md5" ${COPY_DIR}/${BDATE}
+                cp $OUT/${FILENAME} ${COPY_DIR}/${BDATE}/${BDATE}"_"${FILENAME##*/}
+                cp $OUT/"${FILENAME}.md5" ${COPY_DIR}/${BDATE}/${BDATE}"_"${FILENAME##*/}.md5
         done
-#CRI
-lunch oct_d2cri-userdebug && make installclean && make otapackage -j21
-find out/target/product  '(' -name '*ota-eng*.zip' -size +100000k -print0 ')' |
+
+# d2spr
+lunch oct_d2spr-userdebug && make installclean && make otapackage -j$1
+find out/target/product '(' -name '*ota-eng*.zip' -size +100000k ')' -print0 |
         xargs --null md5sum |
         while read CHECKSUM FILENAME
         do
                 echo "${CHECKSUM} ${FILENAME}" >> "${FILENAME}.md5"
-                mv -f ${FILENAME} ${COPY_DIR}/${BDATE}
-                mv -f "${FILENAME}.md5" ${COPY_DIR}/${BDATE}
+                cp $OUT/${FILENAME} ${COPY_DIR}/${BDATE}/${BDATE}"_"${FILENAME##*/}
+                cp $OUT/"${FILENAME}.md5" ${COPY_DIR}/${BDATE}/${BDATE}"_"${FILENAME##*/}.md5
         done
-#SPR
-lunch oct_d2spr-userdebug && make installclean && make otapackage -j21
-find out/target/product  '(' -name '*ota-eng*.zip' -size +100000k -print0 ')' |
+
+# d2usc
+lunch oct_d2usc-userdebug && make installclean && make otapackage -j$1
+find out/target/product '(' -name '*ota-eng*.zip' -size +100000k ')' -print0 |
         xargs --null md5sum |
         while read CHECKSUM FILENAME
         do
                 echo "${CHECKSUM} ${FILENAME}" >> "${FILENAME}.md5"
-                mv -f ${FILENAME} ${COPY_DIR}/${BDATE}
-                mv -f "${FILENAME}.md5" ${COPY_DIR}/${BDATE}
+                cp $OUT/${FILENAME} ${COPY_DIR}/${BDATE}/${BDATE}"_"${FILENAME##*/}
+                cp $OUT/"${FILENAME}.md5" ${COPY_DIR}/${BDATE}/${BDATE}"_"${FILENAME##*/}.md5
         done
-#TMO
-lunch oct_d2tmo-userdebug && make installclean && make otapackage -j21
-find out/target/product  '(' -name '*ota-eng*.zip' -size +100000k -print0 ')' |
+
+# d2att
+lunch oct_d2att-userdebug && make installclean && make otapackage -j$1
+find out/target/product '(' -name '*ota-eng*.zip' -size +100000k ')' -print0 |
         xargs --null md5sum |
         while read CHECKSUM FILENAME
         do
                 echo "${CHECKSUM} ${FILENAME}" >> "${FILENAME}.md5"
-                mv -f ${FILENAME} ${COPY_DIR}/${BDATE}
-                mv -f "${FILENAME}.md5" ${COPY_DIR}/${BDATE}
+                cp $OUT/${FILENAME} ${COPY_DIR}/${BDATE}/${BDATE}"_"${FILENAME##*/}
+                cp $OUT/"${FILENAME}.md5" ${COPY_DIR}/${BDATE}/${BDATE}"_"${FILENAME##*/}.md5
         done
-#USC
-lunch oct_d2usc-userdebug && make installclean && make otapackage -j21
-find out/target/product  '(' -name '*ota-eng*.zip' -size +100000k -print0 ')' |
+
+# d2cri
+lunch oct_d2cri-userdebug && make installclean && make otapackage -j$1
+find out/target/product '(' -name '*ota-eng*.zip' -size +100000k ')' -print0 |
         xargs --null md5sum |
         while read CHECKSUM FILENAME
         do
                 echo "${CHECKSUM} ${FILENAME}" >> "${FILENAME}.md5"
-                mv -f ${FILENAME} ${COPY_DIR}/${BDATE}
-                mv -f "${FILENAME}.md5" ${COPY_DIR}/${BDATE}
+                cp $OUT/${FILENAME} ${COPY_DIR}/${BDATE}/${BDATE}"_"${FILENAME##*/}
+                cp $OUT/"${FILENAME}.md5" ${COPY_DIR}/${BDATE}/${BDATE}"_"${FILENAME##*/}.md5
         done
-        #VZW
-lunch oct_d2vzw-userdebug && make installclean && make otapackage -j21
-find out/target/product  '(' -name '*ota-eng*.zip' -size +100000k -print0 ')' |
+
+# d2tmo
+lunch oct_d2tmo-userdebug && make installclean && make otapackage -j$1
+find out/target/product '(' -name '*ota-eng*.zip' -size +100000k ')' -print0 |
         xargs --null md5sum |
         while read CHECKSUM FILENAME
         do
                 echo "${CHECKSUM} ${FILENAME}" >> "${FILENAME}.md5"
-                mv -f ${FILENAME} ${COPY_DIR}/${BDATE}
-                mv -f "${FILENAME}.md5" ${COPY_DIR}/${BDATE}
+                cp $OUT/${FILENAME} ${COPY_DIR}/${BDATE}/${BDATE}"_"${FILENAME##*/}
+                cp $OUT/"${FILENAME}.md5" ${COPY_DIR}/${BDATE}/${BDATE}"_"${FILENAME##*/}.md5
+        done
+
+# d2mtr
+lunch oct_d2mtr-userdebug && make installclean && make otapackage -j$1
+find out/target/product '(' -name '*ota-eng*.zip' -size +100000k ')' -print0 |
+        xargs --null md5sum |
+        while read CHECKSUM FILENAME
+        do
+                echo "${CHECKSUM} ${FILENAME}" >> "${FILENAME}.md5"
+                cp $OUT/${FILENAME} ${COPY_DIR}/${BDATE}/${BDATE}"_"${FILENAME##*/}
+                cp $OUT/"${FILENAME}.md5" ${COPY_DIR}/${BDATE}/${BDATE}"_"${FILENAME##*/}.md5
         done
