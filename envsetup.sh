677b6ef93979361b33021b2338cd584ca8a3e92e
treken <chupekevin@gmail.com>
2014-01-02 21:19:02 -0500

    Add brunch and and squisher

--- envsetup.sh
+++ envsetup.sh
@@ -13,6 +13,8 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - jgrep:   Greps on all local Java files.
 - resgrep: Greps on all local res/*.xml files.
 - godir:   Go to the directory containing a file.
+- mka:      Builds using SCHED_BATCH on all processors.
+- cmka:     Cleans and builds using mka.
 
 Look at the source to view more functions. The complete list is:
 EOF
@@ -443,6 +445,58 @@ function print_lunch_menu()
     done | column
 
     echo
+}
+
+function brunch()
+{
+    breakfast $*
+    if [ $? -eq 0 ]; then
+mka bacon
+    else
+echo "No such item in brunch menu. Try 'breakfast'"
+        return 1
+    fi
+return $?
+}
+
+function mka() {
+    case `uname -s` in
+        Darwin)
+            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
+            ;;
+        *)
+            schedtool -B -n 1 -e ionice -n 1 make -j$(cat /proc/cpuinfo | grep "^processor" | wc -l) "$@"
+            ;;
+    esac
+}
+
+function breakfast()
+{
+    target=$1
+    OCT_DEVICES_ONLY="true"
+    unset LUNCH_MENU_CHOICES
+    add_lunch_combo full-eng
+    for f in `/bin/ls vendor/oct/vendorsetup.sh 2> /dev/null`
+        do
+echo "including $f"
+            . $f
+        done
+unset f
+
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        lunch
+    else
+echo "z$target" | grep -q "-"
+        if [ $? -eq 0 ]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the OCT model name
+            lunch oct_$target-userdebug
+        fi
+fi
+return $?
 }
 
 function lunch()
