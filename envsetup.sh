9c394886d2ce818b37bae4e4c2d8052cec21b454
Carl Windhausen <cjwindhausen@gmail.com>
2014-01-09 14:59:30 -0500

    undo

--- envsetup.sh
+++ envsetup.sh
@@ -315,7 +315,7 @@ function chooseproduct()
     if [ "x$TARGET_PRODUCT" != x ] ; then
         default_value=$TARGET_PRODUCT
     else
-        default_value=full_base
+        default_value=full
     fi
 
     export TARGET_PRODUCT=
