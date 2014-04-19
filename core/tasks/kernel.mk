d1a25da6363a038e0be2145b7ce995f57d2c883f
broodplank <guichardflorent@gmail.com>
2014-04-19 04:06:41 +0200

    Fix small derp

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -155,7 +155,7 @@ ifeq ($(TARGET_ARCH),arm)
         ifeq ($(HOST_OS),darwin)
             ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/arm/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-"
         else
-            ifeq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN_PREFIX),)
+            ifneq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN_PREFIX),)
               ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN_PREFIX)"
             endif
             ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-"
